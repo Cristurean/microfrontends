1bdca231bdfef97c117da2626719861c6672a234
Andreea Cristurean <andreea.cristurean@hpm.ro>
2023-10-07 17:27:37 +0300

    test

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
+  console.log("something");
   return (
     <div>
       <h1>Hi there!!</h1>
